a05b4d91dee49a66ab6337f58112e1540f37727f
huwei <huweicool@126.com>
2021-03-25 18:09:25 +0800

    change

--- index.js
+++ index.js
@@ -1,15 +1,252 @@
-!function(n,t){"object"==typeof exports&&"undefined"!=typeof module?module.exports=t():"function"==typeof define&&define.amd?define(t):(n="undefined"!=typeof globalThis?globalThis:n||self).Validate=t()}(this,(function(){"use strict";
-/*! *****************************************************************************
-    Copyright (c) Microsoft Corporation.
-
-    Permission to use, copy, modify, and/or distribute this software for any
-    purpose with or without fee is hereby granted.
-
-    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
-    REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
-    AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
-    INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
-    LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
-    OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
-    PERFORMANCE OF THIS SOFTWARE.
-    ***************************************************************************** */function n(n,t,e,r){return new(e||(e=Promise))((function(u,o){function i(n){try{f(r.next(n))}catch(n){o(n)}}function c(n){try{f(r.throw(n))}catch(n){o(n)}}function f(n){var t;n.done?u(n.value):(t=n.value,t instanceof e?t:new e((function(n){n(t)}))).then(i,c)}f((r=r.apply(n,t||[])).next())}))}function t(n,t){var e,r,u,o,i={label:0,sent:function(){if(1&u[0])throw u[1];return u[1]},trys:[],ops:[]};return o={next:c(0),throw:c(1),return:c(2)},"function"==typeof Symbol&&(o[Symbol.iterator]=function(){return this}),o;function c(o){return function(c){return function(o){if(e)throw new TypeError("Generator is already executing.");for(;i;)try{if(e=1,r&&(u=2&o[0]?r.return:o[0]?r.throw||((u=r.return)&&u.call(r),0):r.next)&&!(u=u.call(r,o[1])).done)return u;switch(r=0,u&&(o=[2&o[0],u.value]),o[0]){case 0:case 1:u=o;break;case 4:return i.label++,{value:o[1],done:!1};case 5:i.label++,r=o[1],o=[0];continue;case 7:o=i.ops.pop(),i.trys.pop();continue;default:if(!(u=i.trys,(u=u.length>0&&u[u.length-1])||6!==o[0]&&2!==o[0])){i=0;continue}if(3===o[0]&&(!u||o[1]>u[0]&&o[1]<u[3])){i.label=o[1];break}if(6===o[0]&&i.label<u[1]){i.label=u[1],u=o;break}if(u&&i.label<u[2]){i.label=u[2],i.ops.push(o);break}u[2]&&i.ops.pop(),i.trys.pop();continue}o=t.call(n,i)}catch(n){o=[6,n],r=0}finally{e=u=0}if(5&o[0])throw o[1];return{value:o[0]?o[1]:void 0,done:!0}}([o,c])}}}var e=function(n,t){return Object.prototype.toString.call(n)==="[object "+t+"]"},r=function(n){return e(n,"Null")},u=function(n){return e(n,"Undefined")};var o=function(n){return e(n,"String")},i=function(n){return o(n)&&function(n){return String.prototype.trim?n.trim():n.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g,"")}(n).length>0},c=function(n){return function(n){return e(n,"Boolean")}(n)&&!1===n},f=function(n){return e(n,"Array")},a=function(n){return e(n,"Object")};"undefined"!=typeof globalThis?globalThis:"undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof self&&self;var l,s=function(n,t,e){return n(e={path:t,exports:{},require:function(n,t){return function(){throw new Error("Dynamic requires are not currently supported by @rollup/plugin-commonjs")}(null==t&&e.path)}},e.exports),e.exports}((function(n,t){!function(n){var t=function(n,t){return Object.prototype.toString.call(n)==="[object "+t+"]"},e=function(n){return t(n,"String")},r=function(n){return t(n,"Boolean")},u=function(n){return t(n,"Array")},o=function(n){return"function"==typeof n},i=function(n){return t(n,"Object")},c={},f=[],a=function(){},l=function(n){return n},s=/[^.\[\]]+/g;function h(n,t){if("object"==typeof n&&null!=n)return n[t]}function d(n,t,r,o,i){if(void 0===i&&(i=l),u(t))return t.map((function(t){return d(n,t,r,o,i)}));var c=function(n){return e(n)&&/[\.\[\]]+/g.test(n)}(t)?function(n,t){for(var e=t.match(s)||[],r=n;e.length;)r=h(r,e.shift());return r}(n,t):h(n,t);return o(c)?i(c):r}n.getAny=function(n,t){return d(n,t,void 0,(function(){return!0}))},n.getArray=function(n,t){return d(n,t,f,u)},n.getBoolean=function(n,t){return d(n,t,!1,(function(n){return e(n)&&(n=n.toLowerCase()),r(n)||0===n||1===n||"false"===n||"true"===n}),(function(n){return Boolean(n)}))},n.getFunction=function(n,t){return d(n,t,a,o)},n.getNumber=function(n,t){return d(n,t,0,(function(n){return isFinite(Number(n))}),(function(n){return Number(n)}))},n.getObject=function(n,t){return d(n,t,c,i)},n.getString=function(n,u){return d(n,u,"",(function(n){return e(n)||t(n,"Number")||r(n)}),(function(n){return String(n)}))},Object.defineProperty(n,"__esModule",{value:!0})}(t)}));var h="length",d="notEqual",p="enum",g="phone",b="email",v="IDCard",y="URL",m="",w=function(n){return s.getString(n,"message")||"fail"},A=((l={})["notEmpty"]=function(n,t){return r(n)||u(n)||o(n)&&!i(n)?w(t):m},l[h]=function(n,t){var e=w(t),u=s.getArray(t,"value"),o=s.getNumber(u,"0")||null,i=s.getNumber(u,"1")||null;return 0==u.length||r(o)?e:1==u.length?n.length<o?e:m:r(i)||n.length<o||n.length>i?e:m},l[d]=function(n,t){return n===t.value?m:w(t)},l[p]=function(n,t){var e=t.value||{};return Object.keys(e).findIndex((function(t){return e[t]===n}))>-1?m:w(t)},l[g]=function(n,t){return function(n){return/^1[3456789]\d{9}$/.test(n)}(n)?m:w(t)},l[b]=function(n,t){return function(n){return/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/g.test(n)}(n)?m:w(t)},l[v]=function(n,t){return function(n){return/^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$/.test(n)||/^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])((\d{4})|\d{3}[A-Z])$/.test(n)}(n)?m:w(t)},l[y]=function(n,t){return function(n){return/(http[s]?|ftp):\/\/[^\/\.]+?\..+\w$/i.test(n)}(n)?m:w(t)},l);function j(e,r){return n(this,void 0,void 0,(function(){var u=this;return t(this,(function(o){return[2,new Promise((function(o){return n(u,void 0,void 0,(function(){var n,u,i,c,a,l,s;return t(this,(function(t){switch(t.label){case 0:n=[],u=Object.keys(e),i=0,t.label=1;case 1:return i<u.length?(c=u[i],a=e[c],[4,x(c,a,r)]):[3,4];case 2:(l=t.sent())&&n.push(l),t.label=3;case 3:return i++,[3,1];case 4:return s=function(n){var t={error:"",list:n.length>0?n:null};if(n.length>0){var e=n[0].message;t.error=f(r=e)&&r.length>0?e[0]:e}var r;return t}(n),o(s),[2]}}))}))}))]}))}))}function x(e,r,u){return n(this,void 0,void 0,(function(){var n,o,i,c;return t(this,(function(t){switch(t.label){case 0:return n=null,a(r)?[4,S(e,r,u)]:[3,2];case 1:(c=t.sent())&&(n={key:e,message:[c]}),t.label=2;case 2:if(!f(r))return[3,7];o=[],i=0,t.label=3;case 3:return i<r.length?[4,S(e,r[i],u)]:[3,6];case 4:(c=t.sent())&&o.push(c),t.label=5;case 5:return i++,[3,3];case 6:o.length>0&&(n={key:e,message:o}),t.label=7;case 7:return[2,n]}}))}))}function S(e,o,f){return n(this,void 0,void 0,(function(){var n,a,l,h,d;return t(this,(function(t){switch(t.label){case 0:return n=s.getString(o,"type"),a=s.getString(o,"message")||"fail",l=o.test,h=f[e],function(n){return"function"==typeof n}(l)?[4,l(h,e,o)]:[3,2];case 1:return d=t.sent(),r(d)||u(d)||c(d)?[2,null]:[2,i(d)?d:a];case 2:return A.hasOwnProperty(n)?[2,A[n](h,o)]:[2,null]}}))}))}return function(e,r){return n(this,void 0,void 0,(function(){var u=this;return t(this,(function(o){if(!a(e)||!a(r))throw"The parameters validate the function must be object";return[2,new Promise((function(o){return n(u,void 0,void 0,(function(){var n;return t(this,(function(t){switch(t.label){case 0:return[4,j(e,r)];case 1:return n=t.sent(),o(n),[2]}}))}))}))]}))}))}}));
+!(function (e) {
+  'function' == typeof define && define.amd ? define(e) : e()
+})(function () {
+  'use strict'
+  var e = function (e, n, t, r) {
+      return new (t || (t = Promise))(function (i, u) {
+        function a(e) {
+          try {
+            s(r.next(e))
+          } catch (e) {
+            u(e)
+          }
+        }
+        function o(e) {
+          try {
+            s(r.throw(e))
+          } catch (e) {
+            u(e)
+          }
+        }
+        function s(e) {
+          var n
+          e.done
+            ? i(e.value)
+            : ((n = e.value),
+              n instanceof t
+                ? n
+                : new t(function (e) {
+                    e(n)
+                  })).then(a, o)
+        }
+        s((r = r.apply(e, n || [])).next())
+      })
+    },
+    n = function (e, n) {
+      var t,
+        r,
+        i,
+        u,
+        a = {
+          label: 0,
+          sent: function () {
+            if (1 & i[0]) throw i[1]
+            return i[1]
+          },
+          trys: [],
+          ops: [],
+        }
+      return (
+        (u = { next: o(0), throw: o(1), return: o(2) }),
+        'function' == typeof Symbol &&
+          (u[Symbol.iterator] = function () {
+            return this
+          }),
+        u
+      )
+      function o(u) {
+        return function (o) {
+          return (function (u) {
+            if (t) throw new TypeError('Generator is already executing.')
+            for (; a; )
+              try {
+                if (
+                  ((t = 1),
+                  r &&
+                    (i =
+                      2 & u[0]
+                        ? r.return
+                        : u[0]
+                        ? r.throw || ((i = r.return) && i.call(r), 0)
+                        : r.next) &&
+                    !(i = i.call(r, u[1])).done)
+                )
+                  return i
+                switch (((r = 0), i && (u = [2 & u[0], i.value]), u[0])) {
+                  case 0:
+                  case 1:
+                    i = u
+                    break
+                  case 4:
+                    return a.label++, { value: u[1], done: !1 }
+                  case 5:
+                    a.label++, (r = u[1]), (u = [0])
+                    continue
+                  case 7:
+                    ;(u = a.ops.pop()), a.trys.pop()
+                    continue
+                  default:
+                    if (
+                      !((i = a.trys),
+                      (i = i.length > 0 && i[i.length - 1]) ||
+                        (6 !== u[0] && 2 !== u[0]))
+                    ) {
+                      a = 0
+                      continue
+                    }
+                    if (3 === u[0] && (!i || (u[1] > i[0] && u[1] < i[3]))) {
+                      a.label = u[1]
+                      break
+                    }
+                    if (6 === u[0] && a.label < i[1]) {
+                      ;(a.label = i[1]), (i = u)
+                      break
+                    }
+                    if (i && a.label < i[2]) {
+                      ;(a.label = i[2]), a.ops.push(u)
+                      break
+                    }
+                    i[2] && a.ops.pop(), a.trys.pop()
+                    continue
+                }
+                u = n.call(e, a)
+              } catch (e) {
+                ;(u = [6, e]), (r = 0)
+              } finally {
+                t = i = 0
+              }
+            if (5 & u[0]) throw u[1]
+            return { value: u[0] ? u[1] : void 0, done: !0 }
+          })([u, o])
+        }
+      }
+    },
+    t = require('validate-data-type'),
+    r = t.isObject,
+    i = t.isArray,
+    u = t.isFunction,
+    a = t.isNull,
+    o = t.isUndefined,
+    s = t.isValidString,
+    c = t.isFalse,
+    l = t.isValidArray,
+    f = require('get-safe-value').getString,
+    h = require('./map')
+  function d(t, r) {
+    return e(this, void 0, void 0, function () {
+      var i = this
+      return n(this, function (u) {
+        return [
+          2,
+          new Promise(function (u) {
+            return e(i, void 0, void 0, function () {
+              var e, i, a, o, s, c, f
+              return n(this, function (n) {
+                switch (n.label) {
+                  case 0:
+                    ;(e = []), (i = Object.keys(t)), (a = 0), (n.label = 1)
+                  case 1:
+                    return a < i.length
+                      ? ((o = i[a]), (s = t[o]), [4, v(o, s, r)])
+                      : [3, 4]
+                  case 2:
+                    ;(c = n.sent()) && e.push(c), (n.label = 3)
+                  case 3:
+                    return a++, [3, 1]
+                  case 4:
+                    return (
+                      (f = (function (e) {
+                        var n = { error: '', list: e.length > 0 ? e : null }
+                        if (e.length > 0) {
+                          var t = e[0].message
+                          n.error = l(t) ? t[0] : t
+                        }
+                        return n
+                      })(e)),
+                      u(f),
+                      [2]
+                    )
+                }
+              })
+            })
+          }),
+        ]
+      })
+    })
+  }
+  function v(t, u, a) {
+    return e(this, void 0, void 0, function () {
+      var e, o, s, c
+      return n(this, function (n) {
+        switch (n.label) {
+          case 0:
+            return (e = null), r(u) ? [4, b(t, u, a)] : [3, 2]
+          case 1:
+            ;(c = n.sent()) && (e = { key: t, message: [c] }), (n.label = 2)
+          case 2:
+            if (!i(u)) return [3, 7]
+            ;(o = []), (s = 0), (n.label = 3)
+          case 3:
+            return s < u.length ? [4, b(t, u[s], a)] : [3, 6]
+          case 4:
+            ;(c = n.sent()) && o.push(c), (n.label = 5)
+          case 5:
+            return s++, [3, 3]
+          case 6:
+            o.length > 0 && (e = { key: t, message: o }), (n.label = 7)
+          case 7:
+            return [2, e]
+        }
+      })
+    })
+  }
+  function b(t, r, i) {
+    return e(this, void 0, void 0, function () {
+      var e, l, d, v, b
+      return n(this, function (n) {
+        switch (n.label) {
+          case 0:
+            return (
+              (e = f(r, 'type')),
+              (l = f(r, 'message') || 'fail'),
+              (d = r.test),
+              (v = i[t]),
+              u(d) ? [4, d(v, t, r)] : [3, 2]
+            )
+          case 1:
+            return (
+              (b = n.sent()),
+              a(b) || o(b) || c(b) ? [2, null] : [2, s(b) ? b : l]
+            )
+          case 2:
+            return h.hasOwnProperty(e) ? [2, h[e](v, r)] : [2, null]
+        }
+      })
+    })
+  }
+  module.exports = function (t, i) {
+    return e(this, void 0, void 0, function () {
+      var u = this
+      return n(this, function (a) {
+        if (!r(t) || !r(i))
+          throw 'The parameters Validate the function must be object'
+        return [
+          2,
+          new Promise(function (r) {
+            return e(u, void 0, void 0, function () {
+              var e
+              return n(this, function (n) {
+                switch (n.label) {
+                  case 0:
+                    return [4, d(t, i)]
+                  case 1:
+                    return (e = n.sent()), r(e), [2]
+                }
+              })
+            })
+          }),
+        ]
+      })
+    })
+  }
+})
